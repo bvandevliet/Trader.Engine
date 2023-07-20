2badd8fd0e59e0bcc2b16ddb4c20341e95d507d7
Bob Vandevliet <info@bvandevliet.nl>
2023-07-20 09:52:12 +0200

    Add project files.

---
+++ TraderEngine.API/Program.cs
@@ -0,0 +1,29 @@
+namespace TraderEngine.API;
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddControllers();
+
+    // Swagger/OpenAPI.
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+      app.UseSwagger();
+      app.UseSwaggerUI();
+    }
+
+    //app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+  }
+}
---
+++ TraderEngine.CLI/Program.cs
@@ -0,0 +1,9 @@
+namespace TraderEngine.CLI;
+
+internal class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello, World!");
+  }
+}
