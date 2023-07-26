5ca9c988befe9baebb4c1a6e534c7e44cea14ae2
Bob Vandevliet <info@bvandevliet.nl>
2023-07-26 17:46:57 +0200

    Added exchange and market cap interfaces.

---
+++ TraderEngine.API/Services/IExchange.cs
@@ -0,0 +1,44 @@
+using TraderEngine.Common.DTOs.Request;
+using TraderEngine.Common.DTOs.Response;
+using TraderEngine.Common.Enums;
+using TraderEngine.Common.Models;
+
+namespace TraderEngine.API.Services;
+
+public interface IExchange
+{
+  string QuoteSymbol { get; }
+
+  decimal MinimumOrderSize { get; }
+
+  decimal MakerFee { get; }
+
+  decimal TakerFee { get; }
+
+  Task<Balance> GetBalance();
+
+  // TODO: ASIGN TYPE !!
+  Task<object> DepositHistory();
+
+  // TODO: ASIGN TYPE !!
+  Task<object> WithdrawHistory();
+
+  // TODO: ASIGN TYPE !!
+  Task<object> GetCandles(MarketDto market, CandleInterval interval, int limit);
+
+  Task<bool> IsTradable(MarketDto market);
+
+  Task<decimal> GetPrice(MarketDto market);
+
+  Task<Order> NewOrder(OrderDto order);
+
+  Task<Order?> GetOrder(string orderId, MarketDto? market = null);
+
+  Task<Order?> CancelOrder(string orderId, MarketDto? market = null);
+
+  Task<IEnumerable<Order>> GetOpenOrders(MarketDto? market = null);
+
+  Task<IEnumerable<Order>> CancelAllOpenOrders(MarketDto? market = null);
+
+  Task<IEnumerable<Order>> SellAllPositions(string? baseSymbol = null);
+}
---
+++ TraderEngine.Common/Services/IMarketCapInternalRepository.cs
@@ -0,0 +1,40 @@
+using TraderEngine.Common.DTOs.Request;
+using TraderEngine.Common.DTOs.Response;
+
+namespace TraderEngine.Common.Services;
+
+/// <summary>
+/// Interacts with the internal market cap database.
+/// </summary>
+public interface IMarketCapInternalRepository
+{
+  /// <summary>
+  /// Saves a market cap object to the database.
+  /// </summary>
+  /// <param name="marketCap"></param>
+  /// <returns></returns>
+  Task Insert(MarketCapData marketCap);
+
+  /// <summary>
+  /// Saves multiple market cap objects to the database.
+  /// </summary>
+  /// <param name="marketCaps"></param>
+  /// <returns></returns>
+  Task InsertMany(IEnumerable<MarketCapData> marketCaps);
+
+  /// <summary>
+  /// Get all historical market cap data for the specified market within given amount of days ago.
+  /// </summary>
+  /// <param name="market"></param>
+  /// <param name="days"></param>
+  /// <returns></returns>
+  Task<IEnumerable<MarketCapData>> ListHistorical(MarketDto market, int days = 21);
+
+  /// <summary>
+  /// Get all historical market cap data of top ranked base currencies for the specified quote currency within given amount of days ago.
+  /// </summary>
+  /// <param name="quoteSymbol"></param>
+  /// <param name="days"></param>
+  /// <returns></returns>
+  IAsyncEnumerable<IEnumerable<MarketCapData>> ListHistoricalMany(string quoteSymbol, int days = 21);
+}
