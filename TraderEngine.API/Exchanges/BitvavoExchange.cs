e542681b156dcec9e9ad39b9e38b9710b923ed0d
Bob Vandevliet <info@bvandevliet.nl>
2023-08-02 08:01:03 +0200

    Corrected the request signature.

--- TraderEngine.API/Exchanges/BitvavoExchange.cs
+++ TraderEngine.API/Exchanges/BitvavoExchange.cs
@@ -33,47 +33,54 @@ public class BitvavoExchange : IExchange
 
   private string CreateSignature(long timestamp, string method, string url, object? body)
   {
-    var sb = new StringBuilder();
+    var inputStrBuilder = new StringBuilder();
 
-    sb.Append(timestamp).Append(method).Append("/v2").Append(url);
+    inputStrBuilder.Append(timestamp).Append(method).Append(url);
 
     if (body != null)
     {
       string bodyJson = JsonSerializer.Serialize(body);
 
-      sb.Append(bodyJson);
+      inputStrBuilder.Append(bodyJson);
     }
 
     using var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(ApiSecret));
 
-    byte[] inputBytes = Encoding.UTF8.GetBytes(sb.ToString());
+    byte[] inputBytes = Encoding.UTF8.GetBytes(inputStrBuilder.ToString());
 
     byte[] signatureBytes = hmac.ComputeHash(inputBytes);
 
-    string signature = BitConverter.ToString(signatureBytes);
+    var outputStrBuilder = new StringBuilder(signatureBytes.Length * 2);
 
-    return signature;
+    foreach (byte b in signatureBytes)
+    {
+      outputStrBuilder.Append(b.ToString("x2"));
+    }
+
+    return outputStrBuilder.ToString();
   }
 
-  private void AddOneTimeHeaders(HttpRequestMessage request, object? body = null)
+  private HttpRequestMessage CreateRequestMsg(HttpMethod method, string requestPath, object? body = null)
   {
+    var request = new HttpRequestMessage(method, new Uri(_httpClient.BaseAddress!, requestPath));
+
     request.Headers.Add(HeaderNames.Accept, "application/json");
     request.Headers.Add("bitvavo-access-window", "60000 ");
 
     long timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
 
-    string signature = CreateSignature(timestamp, request.Method.ToString(), request.RequestUri!.AbsolutePath, body);
+    string signature = CreateSignature(timestamp, request.Method.ToString(), request.RequestUri!.PathAndQuery, body);
 
     request.Headers.Add("bitvavo-access-key", ApiKey);
     request.Headers.Add("bitvavo-access-timestamp", timestamp.ToString());
     request.Headers.Add("bitvavo-access-signature", signature);
+
+    return request;
   }
 
   public async Task<Balance> GetBalance()
   {
-    using var request = new HttpRequestMessage(HttpMethod.Get, "balance");
-
-    AddOneTimeHeaders(request);
+    using var request = CreateRequestMsg(HttpMethod.Get, "balance");
 
     var response = await _httpClient.SendAsync(request);
 
@@ -131,10 +138,8 @@ public class BitvavoExchange : IExchange
 
   public async Task<decimal> GetPrice(MarketReqDto market)
   {
-    using var request = new HttpRequestMessage(HttpMethod.Get,
-      $"ticker/price?market={market.BaseSymbol}-{market.QuoteSymbol}");
-
-    AddOneTimeHeaders(request);
+    using var request = CreateRequestMsg(
+      HttpMethod.Get, $"ticker/price?market={market.BaseSymbol}-{market.QuoteSymbol}");
 
     var response = await _httpClient.SendAsync(request);
 
