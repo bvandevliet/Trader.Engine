dcbe5e87e11cd1069e071fbdbe0f1599a8f1b516
Bob Vandevliet <info@bvandevliet.nl>
2023-11-17 09:28:50 +0100

    Removed unused method.

--- TraderEngine.Common/Helpers/RebalanceHelpers.cs
+++ TraderEngine.Common/Helpers/RebalanceHelpers.cs
@@ -1,5 +1,4 @@
 using TraderEngine.Common.DTOs.API.Request;
-using TraderEngine.Common.DTOs.API.Response;
 using TraderEngine.Common.Models;
 
 namespace TraderEngine.Common.Helpers;
@@ -13,7 +12,6 @@ public static class RebalanceHelpers
   /// <param name="newAbsAllocs"></param>
   /// <param name="curBalance"></param>
   /// <returns>Collection of current <see cref="Allocation"/>s and their deviation in quote currency.</returns>
-  // TODO: Make DRY !!
   public static IEnumerable<AllocDiffReqDto> GetAllocationQuoteDiffs(IEnumerable<AbsAllocReqDto> newAbsAllocs, Balance curBalance)
   {
     // Initialize absolute asset allocation List,
@@ -25,13 +23,13 @@ public static class RebalanceHelpers
     decimal totalAbsAlloc = 0;
 
     // Multi-purpose foreach to eliminate redundant iterations.
-    foreach (var absAssetAlloc in newAbsAllocs)
+    foreach (var absAlloc in newAbsAllocs)
     {
       // Add to sum of all absolute allocation values.
-      totalAbsAlloc += absAssetAlloc.AbsAlloc;
+      totalAbsAlloc += absAlloc.AbsAlloc;
 
       // Add to absolute asset allocation List.
-      newAbsAllocsList.Add(absAssetAlloc);
+      newAbsAllocsList.Add(absAlloc);
     }
 
     // Loop through current allocations and determine quote diffs.
@@ -39,7 +37,7 @@ public static class RebalanceHelpers
     {
       // Find associated absolute allocation.
       decimal absAlloc =
-        newAbsAllocsList.Find(absAssetAlloc => absAssetAlloc.BaseSymbol.Equals(curAlloc.Market.BaseSymbol))?.AbsAlloc ?? 0;
+        newAbsAllocsList.Find(absAlloc => absAlloc.BaseSymbol.Equals(curAlloc.Market.BaseSymbol))?.AbsAlloc ?? 0;
 
       // Determine relative allocation.
       decimal relAlloc = totalAbsAlloc == 0 ? 0 : absAlloc / totalAbsAlloc;
@@ -55,93 +53,19 @@ public static class RebalanceHelpers
     }
 
     // Loop through absolute asset allocations and determine yet missing quote diffs.
-    foreach (var absAssetAlloc in newAbsAllocsList)
+    foreach (var absAlloc in newAbsAllocsList)
     {
-      if (null != curBalance.GetAllocation(absAssetAlloc.BaseSymbol))
+      if (null != curBalance.GetAllocation(absAlloc.BaseSymbol))
       {
         // Already covered in previous foreach.
         continue;
       }
 
       // Define current allocation, which is zero here.
-      Allocation curAlloc = new(new MarketReqDto(curBalance.QuoteSymbol, absAssetAlloc.BaseSymbol), 0, 0);
+      Allocation curAlloc = new(new MarketReqDto(curBalance.QuoteSymbol, absAlloc.BaseSymbol), 0, 0);
 
       // Determine relative allocation.
-      decimal relAlloc = totalAbsAlloc == 0 ? 0 : absAssetAlloc.AbsAlloc / totalAbsAlloc;
-
-      // Determine new quote amount.
-      decimal newAmountQuote = relAlloc * curBalance.AmountQuoteTotal;
-
-      yield return new AllocDiffReqDto(
-        curAlloc.Market,
-        curAlloc.Price,
-        curAlloc.Amount,
-        -newAmountQuote);
-    }
-  }
-
-  /// <summary>
-  /// Get current deviation in quote currency when comparing absolute new allocations in
-  /// <paramref name="newAbsAllocs"/> against current allocations in <paramref name="curBalance"/>.
-  /// </summary>
-  /// <param name="newAbsAllocs"></param>
-  /// <param name="curBalance"></param>
-  /// <returns>Collection of current <see cref="Allocation"/>s and their deviation in quote currency.</returns>
-  // TODO: Make DRY !!
-  public static IEnumerable<AllocDiffReqDto> GetAllocationQuoteDiffs(IEnumerable<AbsAllocReqDto> newAbsAllocs, BalanceDto curBalance)
-  {
-    // Initialize absolute asset allocation List,
-    // being filled using a multi-purpose foreach to eliminate redundant iterations.
-    List<AbsAllocReqDto> newAbsAllocsList = new();
-
-    // Sum of all absolute allocation values.
-    // being summed up using a multi-purpose foreach to eliminate redundant iterations.
-    decimal totalAbsAlloc = 0;
-
-    // Multi-purpose foreach to eliminate redundant iterations.
-    foreach (var absAssetAlloc in newAbsAllocs)
-    {
-      // Add to sum of all absolute allocation values.
-      totalAbsAlloc += absAssetAlloc.AbsAlloc;
-
-      // Add to absolute asset allocation List.
-      newAbsAllocsList.Add(absAssetAlloc);
-    }
-
-    // Loop through current allocations and determine quote diffs.
-    foreach (var curAlloc in curBalance.Allocations)
-    {
-      // Find associated absolute allocation.
-      decimal absAlloc =
-        newAbsAllocsList.Find(absAssetAlloc => absAssetAlloc.BaseSymbol.Equals(curAlloc.Market.BaseSymbol))?.AbsAlloc ?? 0;
-
-      // Determine relative allocation.
-      decimal relAlloc = totalAbsAlloc == 0 ? 0 : absAlloc / totalAbsAlloc;
-
-      // Determine new quote amount.
-      decimal newAmountQuote = relAlloc * curBalance.AmountQuoteTotal;
-
-      yield return new AllocDiffReqDto(
-        curAlloc.Market,
-        curAlloc.Price,
-        curAlloc.Amount,
-        curAlloc.AmountQuote - newAmountQuote);
-    }
-
-    // Loop through absolute asset allocations and determine yet missing quote diffs.
-    foreach (var absAssetAlloc in newAbsAllocsList)
-    {
-      if (null != curBalance.Allocations.Find(alloc => alloc.Market.BaseSymbol.Equals(absAssetAlloc.BaseSymbol)))
-      {
-        // Already covered in previous foreach.
-        continue;
-      }
-
-      // Define current allocation, which is zero here.
-      Allocation curAlloc = new(new MarketReqDto(curBalance.QuoteSymbol, absAssetAlloc.BaseSymbol), 0, 0);
-
-      // Determine relative allocation.
-      decimal relAlloc = totalAbsAlloc == 0 ? 0 : absAssetAlloc.AbsAlloc / totalAbsAlloc;
+      decimal relAlloc = totalAbsAlloc == 0 ? 0 : absAlloc.AbsAlloc / totalAbsAlloc;
 
       // Determine new quote amount.
       decimal newAmountQuote = relAlloc * curBalance.AmountQuoteTotal;
